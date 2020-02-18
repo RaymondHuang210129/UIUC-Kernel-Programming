9e746d2e60a4dd3fabf78d0519cf58c1a2ea837b
RaymondHuang210129 <raymond860909@gmail.com>
2020-02-18 12:07:06 -0800

    calculate factorial in userapp

--- mp1-cputime/userapp.c
+++ mp1-cputime/userapp.c
@@ -7,12 +7,14 @@ int main(int argc, char* argv[])
 	char * line_buf = NULL;
 	size_t line_buf_size = 0;
 	ssize_t line_size;
+	int i;
+	unsigned long factorial = 1;
 	fp = fopen("/proc/mp1/status", "w");
 	fprintf(fp, "%d", pid);
 	fclose(fp);
 	unsigned long int start_time = (unsigned)time(NULL);
-	while(1) 
-	{
+	for (i = 1;; i++) {
+		factorial *= i;
 		unsigned long int current_time = (unsigned)time(NULL);
 		if (current_time - start_time >= 20) break;  
 	}
